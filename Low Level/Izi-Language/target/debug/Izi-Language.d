26e8fcb5a215cc086ec92209d3060ea4d902aa05
GabrielFrigo <66432268+GabrielFrigo4@users.noreply.github.com>
2024-04-05 16:29:16 -0300

    Remove Target

--- Low Level/Izi-Language/target/debug/Izi-Language.d
+++
@@ -1,1 +0,0 @@
-/home/gabriel/Documentos/GitHub/GabrielFrigo4/Code-GTHC/Low\ Level/Izi-Language/target/debug/Izi-Language: /home/gabriel/Documentos/GitHub/GabrielFrigo4/Code-GTHC/Low\ Level/Izi-Language/src/main.rs
--- Low Level/Izi-Language/target/debug/deps/Izi_Language-f1f6f4a5abd7f091.d
+++
@@ -1,5 +0,0 @@
-/home/gabriel/Documentos/GitHub/GabrielFrigo4/Code-GTHC/Low Level/Izi-Language/target/debug/deps/libIzi_Language-f1f6f4a5abd7f091.rmeta: src/main.rs
-
-/home/gabriel/Documentos/GitHub/GabrielFrigo4/Code-GTHC/Low Level/Izi-Language/target/debug/deps/Izi_Language-f1f6f4a5abd7f091.d: src/main.rs
-
-src/main.rs:
